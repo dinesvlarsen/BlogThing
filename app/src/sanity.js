3fb094ed80729f949522f6daaceb6fdbc8ef3f82
dinesvlarsen <dinesvlarsen@gmail.com>
2022-05-18 14:45:43 +0200

    added credentials to the client

--- app/src/sanity.js
+++ app/src/sanity.js
@@ -4,5 +4,6 @@ export default sanityClient({
 	projectId: '4u8ecpnw',
 	dataset: 'production',
 	apiVersion: '2022-05-11', //dagen prosjektet ble først opprettet/sist oppdatert
-	useCdn: false //false for localhost, true for netlify
+	useCdn: false, //false for localhost, true for netlify
+	withCredentials: true
 });
