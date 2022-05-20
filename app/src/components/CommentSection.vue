634f73f2c12825ed045d199e93481aa4d76dddaa
dinesvlarsen <dinesvlarsen@gmail.com>
2022-05-20 19:14:43 +0200

    Added logic to create and render commentse

--- app/src/components/CommentSection.vue
+++ app/src/components/CommentSection.vue
@@ -1,7 +1,7 @@
 <template>
 	<form
 		id="form"
-		@submit.prevent="checkForm"
+		@submit.prevent="submit"
 		method="post"
 		action="https://sanity.io"
 	>
@@ -15,32 +15,77 @@
 		<button type="submit">submit</button>
 	</form>
 
+	<div>
+		<div v-for="comment in localComments" :key="comment._id">
+			<h2>{{ comment.name }}</h2>
+			<hr />
+			<p>{{ comment.text }}</p>
+		</div>
+	</div>
 	<hr />
 </template>
 
 <script>
-const comment_key = import.meta.env.VITE_APP_SANITY_COMMENT;
-
 import sanity from './../sanity.js';
+import commentsQuery from './../groq/comments.groq?raw';
 
 export default {
+	props: ['id', 'comments'],
+	mounted() {
+		//Doing this because I need my prop to be mutable, solution taken from https://stackoverflow.com/questions/69225771/are-the-props-in-a-vue-component-mutable.
+		//Might have to refactor this approach and move the query for comments into store, or query for the data needed in this component directly inside it, instead of passing the data down from BlogArticlePage.vue.
+		this.localComments = [...this.comments];
+	},
+	created() {
+		// console.log(this.$route.params.projectSlug);
+	},
 	data() {
 		return {
 			form: {
 				name: '',
 				textArea: '',
+				id: this.id,
 			},
+			localComments: [],
 		};
 	},
 	methods: {
 		submit() {
-			console.log('click');
+			// console.log('form made?');
 			this.createComment(this.form);
+			this.queryForComments();
 		},
 		createComment({ name, textArea }) {
-			console.log(name, textArea);
+			// console.log(name, textArea);
+			sanity.create({
+				_type: 'comment',
+				name: this.form.name,
+				text: this.form.textArea,
+				post: {
+					_type: 'reference',
+					_ref: this.form.id,
+				},
+			});
+		},
+		async queryForComments() {
+			await sanity
+				.fetch(commentsQuery, {
+					slug: this.$route.params.projectSlug,
+				})
+				.then((data) => {
+					console.log(data);
+					this.localComments = data;
+					this.loading = false;
+					// blocks = this.result.body;
+					// //Only want to get blocks if we enter a blog post page.
+					// if (params.slug) {
+					// 	this.blocks = this.result.body;
+					// 	this.id = this.result._id;
+					// }
+				});
 		},
 	},
+	computed: {},
 };
 </script>
 
