cefacf13edd4f18d31f68b3ae6e4e1271903428b
dinesvlarsen <dinesvlarsen@gmail.com>
2022-05-25 11:21:22 +0200

    removed scroll to top method from articles

--- app/src/components/LatestArticles.vue
+++ app/src/components/LatestArticles.vue
@@ -5,7 +5,7 @@
 				:src="article.coverImage.image.asset.url"
 				:alt="article.coverImage.alt"
 			/>
-			<router-link @click="scrollToTop" :to="article.slug.current">
+			<router-link :to="article.slug.current">
 				<h2>{{ article.title }}</h2>
 			</router-link>
 			<!-- Need to change these to router-link, but couldn't figure out what was causing the router-link to not work -->
@@ -19,10 +19,6 @@ export default {
 	props: ['articles'],
 	mixins: [viewMixin],
 
-	methods: {
-		scrollToTop() {
-			window.scrollTo(0, 0);
-		},
-	},
+	methods: {},
 };
 </script>
