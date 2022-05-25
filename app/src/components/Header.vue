a5d42ae0e3cd45b76b337634791025cfab83906f
dinesvlarsen <dinesvlarsen@gmail.com>
2022-05-25 15:39:45 +0200

    Added styling to the header component

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,22 +1,27 @@
 <template>
-	<header>
-		<nav>
-			<ul>
-				<RouterLink to="/">
-					<div class="logo" aria-label="logo">
-						Blog<span class="logo__detail">Thing</span>
-					</div>
-				</RouterLink>
-			</ul>
-		</nav>
+	<header class="header">
+		<RouterLink to="/">
+			<div class="header__logo" aria-label="logo">
+				Blog<span class="logo__detail">Thing</span>
+			</div>
+		</RouterLink>
 	</header>
 </template>
 <style>
-.logo {
+.header {
+	display: flex;
+	justify-content: center;
+	padding-top: 16px;
+	margin-bottom: 32px;
+	/* Dont want the logo to be root size,  */
+	font-size: 16px;
+}
+
+.header__logo {
 	font-weight: bold;
-	color: hsl(284, 90, 45);
 }
 
 .logo__detail {
+	color: var(--accent);
 }
 </style>
