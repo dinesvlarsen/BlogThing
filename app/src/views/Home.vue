2091b71cd12a028b30a73626115a4b63de938848
dinesvlarsen <dinesvlarsen@gmail.com>
2022-05-25 15:40:17 +0200

    added some home styling

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,13 +1,59 @@
 <template>
 	<Loading v-if="loading" />
-	<div v-else="loading" v-for="post in result">
-		<img :src="this.imageURL(post)" :alt="post.coverImage.alt" />
-		<h1>{{ post.title }}</h1>
-		<time :datetime="post.date">{{ post.date }}</time>
-		<p>{{ post.description }}</p>
-		<RouterLink :to="post.slug.current">more -></RouterLink>
-	</div>
+
+	<main class="post" v-else="loading" v-for="post in result">
+		<img
+			class="post__image"
+			:src="this.imageURL(post)"
+			:alt="post.coverImage.alt"
+		/>
+		<h1 class="post__header">{{ post.title }}</h1>
+		<time class="post__time" :datetime="post.date">{{ post.date }}</time>
+		<p class="post__description">{{ post.description }}</p>
+		<RouterLink class="post__link" :to="post.slug.current">more -></RouterLink>
+	</main>
 </template>
+
+<style>
+.post > *:not(img) {
+	margin-left: 32px;
+	margin-right: 32px;
+}
+
+.post {
+	margin-bottom: 6rem;
+}
+
+.post__image,
+.post__description {
+	margin-bottom: 24px;
+}
+
+.post__header {
+	margin-bottom: 4px;
+	font-size: 1.5rem;
+}
+
+.post__time {
+	display: inline-block;
+	margin-bottom: 16px;
+	color: var(--secondary);
+	font-size: 0.875rem;
+}
+
+.post__link {
+	color: var(--secondary);
+}
+
+.post__link:visited {
+	color: var(--secondary);
+}
+
+.post__link:active {
+	color: var(--accent);
+	text-decoration: underline;
+}
+</style>
 
 <script>
 import query from '../groq/home.groq?raw';
